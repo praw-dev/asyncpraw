praw-dev/praw

Adds the "subreddit" parameter to Reddit.info(). (#1570)
This change adds the `subreddit` parameter as defined in the reddit API documentation (https://www.reddit.com/dev/api#GET_api_info). This change was implemented in the Reddit API earlier today, which makes it easier for developers to get many subreddits at once by their name, which can significantly reduce the amount of requests made by an API client.

This change adds the parameter to info, updates relevant checks, and updates the test that test for IDs provided. Previously, if developers wanted to use this new parameter they would have to perform the request manually.

In accordance with some contributing guidelines, this commit adds the two integration tests needed.

The test uses one subreddit name that is normally formatted (/r/redditdev) and three others, which while are still valid subreddits, are different from the norm in some way (/r/nl's name is too short, /r/t:1337 is a timereddit which has a colon in its subreddit name, and /r/reddit.com which has a period in its name.

Each subreddit exists and is publicly accessible, which should make tests that use these subreddit succeed for the forseeable future. If the visibility of any of the subreddits do change, the test may have to be changed to reflect this.

tests/integration/test_reddit.py
@@ -7,6 +7,7 @@ from prawcore.exceptions import BadRequest
 from praw.models import LiveThread
 from praw.models.reddit.base import RedditBase
 from praw.models.reddit.submission import Submission
+from praw.models.reddit.subreddit import Subreddit
 
 from . import IntegrationTest
 
@@ -46,6 +47,15 @@ class TestReddit(IntegrationTest):
         assert len(results) > 0
         for item in results:
             assert isinstance(item, Submission)
+
+    def test_info_sr_names(self):
+        items = [self.reddit.subreddit("redditdev"), "reddit.com", "t:1337", "nl"]
+        item_generator = self.reddit.info(subreddits=items)
+        with self.recorder.use_cassette("TestReddit.test_info_sr_names"):
+            results = list(item_generator)
+        assert len(results) == 4
+        for item in results:
+            assert isinstance(item, Subreddit)
 
     @mock.patch("time.sleep", return_value=None)
     def test_live_call(self, _):

praw-dev/asyncpraw

(cherry picked from 0e57527d3f5ccc0279f6b0c21b310b1f607f8152)

tests/integration/test_reddit.py
@@ -7,6 +7,7 @@ from asyncprawcore.exceptions import BadRequest
 from asyncpraw.models import LiveThread
 from asyncpraw.models.reddit.base import RedditBase
 from asyncpraw.models.reddit.submission import Submission
+from asyncpraw.models.reddit.subreddit import Subreddit
 
 from . import IntegrationTest
 
@@ -45,6 +46,15 @@ class TestReddit(IntegrationTest):
         assert len(results) > 0
         for item in results:
             assert isinstance(item, Submission)
+
+    async def test_info_sr_names(self):
+        items = [await self.reddit.subreddit("redditdev"), "reddit.com", "t:1337", "nl"]
+        item_generator = self.reddit.info(subreddits=items)
+        with self.use_cassette():
+            results = await self.async_list(item_generator)
+        assert len(results) == 4
+        for item in results:
+            assert isinstance(item, Subreddit)
 
     @mock.patch("asyncio.sleep", return_value=None)
     async def test_live_call(self, _):