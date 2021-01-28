praw-dev/praw

Return websocket URL from media upload
As explained in the previous commit message (4f26546),
Reddit now returns a websocket URL with our media upload. We return it
here and update some callers to ignore it.

tests/unit/models/reddit/test_subreddit.py
@@ -55,10 +55,12 @@ class TestSubreddit(UnitTest):
         assert hash(subreddit1) != hash(subreddit3)
 
     @mock.patch(
-        "praw.Reddit.post",
-        return_value={"json": {"data": {"websocket_url": ""}}},
+        "praw.Reddit.post", return_value={"json": {"data": {"websocket_url": ""}}}
     )
-    @mock.patch("praw.models.Subreddit._upload_media", return_value="")
+    @mock.patch(
+        "praw.models.Subreddit._upload_media",
+        return_value=("fake_media_url", "fake_websocket_url"),
+    )
     @mock.patch("websocket.create_connection")
     def test_invalid_media(self, connection_mock, _mock_upload_media, _mock_post):
         connection_mock().recv.return_value = json.dumps(

praw-dev/asyncpraw

(cherry picked from commit c6f6260405aeef0d90044b9b471b4a18373a2d5d)

tests/unit/models/reddit/test_subreddit.py
@@ -54,10 +54,12 @@ class TestSubreddit(UnitTest):
         assert hash(subreddit1) != hash(subreddit3)
 
     @mock.patch(
-        "asyncpraw.Reddit.post",
-        return_value={"json": {"data": {"websocket_url": ""}}},
+        "asyncpraw.Reddit.post", return_value={"json": {"data": {"websocket_url": ""}}}
     )
-    @mock.patch("asyncpraw.models.Subreddit._upload_media", return_value="")
+    @mock.patch(
+        "asyncpraw.models.Subreddit._upload_media",
+        return_value=("fake_media_url", "fake_websocket_url"),
+    )
     @mock.patch("aiohttp.client.ClientSession.ws_connect")
     async def test_invalid_media(self, connection_mock, _mock_upload_media, _mock_post):
         self.reddit._core._requestor._http = aiohttp.ClientSession()