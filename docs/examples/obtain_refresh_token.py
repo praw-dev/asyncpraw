4deed943c17cc3f6a0766b820f76eba537a64706
Bryce Boe <bbzbryce@gmail.com>
2021-02-23 18:32:43 -0600

    Update existing scripts in /docs/examples
    (cherry picked from commit bb0e00ee0068442b0649ff3e356ed1b9d9922e44)

--- docs/examples/obtain_refresh_token.py
+++ docs/examples/obtain_refresh_token.py
@@ -2,11 +2,27 @@
 
 """This example demonstrates the flow for retrieving a refresh token.
 
-In order for this example to work your application's redirect URI must be set
-to http://localhost:8080.
+
 
 This tool can be used to conveniently create refresh tokens for later use with
 your web application OAuth2 credentials.
+
+To create a Reddit application visit the following link while logged into the account
+you want to create a refresh token for: https://www.reddit.com/prefs/apps/
+
+Create a "web app" with the redirect uri set to: http://localhost:8080
+
+After the application is created, take note of:
+
+- REDDIT_CLIENT_ID; the line just under "web app" in the upper left of the Reddit
+  Application
+- REDDIT_CLIENT_SECRET; the value to the right of "secret"
+
+Usage:
+
+    EXPORT praw_client_id=<REDDIT_CLIENT_ID>
+    EXPORT praw_client_secret=<REDDIT_CLIENT_SECRET>
+    python3 obtain_refresh_token.py
 
 """
 import asyncio
@@ -17,62 +33,20 @@ import sys
 import asyncpraw
 
 
-def receive_connection():
-    """Wait for and then return a connected socket..
-
-    Opens a TCP connection on port 8080, and waits for a single client.
-
-    """
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(("localhost", 8080))
-    server.listen(1)
-    client = server.accept()[0]
-    server.close()
-    return client
-
-
-def send_message(client, message):
-    """Send message to client and close the connection."""
-    print(message)
-    client.send(f"HTTP/1.1 200 OK\r\n\r\n{message}".encode("utf-8"))
-    client.close()
-
-
 async def main():
     """Provide the program's entry point when directly executed."""
-    print(
-        "Go here while logged into the account you want to create a token for:"
-        " https://www.reddit.com/prefs/apps/"
+    scope_input = input(
+        "Enter a comma separated list of scopes, or `*` for all scopes: "
     )
-    print(
-        "Click the create an app button. Put something in the name field and select the"
-        " script radio button."
-    )
-    print("Put http://localhost:8080 in the redirect uri field and click create app")
-    client_id = input(
-        "Enter the client ID, it's the line just under Personal use script at the top: "
-    )
-    client_secret = input("Enter the client secret, it's the line next to secret: ")
-    commaScopes = input(
-        "Now enter a comma separated list of scopes, or all for all tokens: "
-    )
-
-    if commaScopes.lower() == "all":
-        scopes = ["*"]
-    else:
-        scopes = commaScopes.strip().split(",")
+    scopes = [scope.strip() for scope in scope_input.strip().split(",")]
 
     reddit = asyncpraw.Reddit(
-        client_id=client_id.strip(),
-        client_secret=client_secret.strip(),
         redirect_uri="http://localhost:8080",
-        user_agent="praw_refresh_token_example",
+        user_agent="obtain_refresh_token/v0 by u/bboe",
     )
     state = str(random.randint(0, 65000))
     url = reddit.auth.url(scopes, state, "permanent")
     print(f"Now open this url in your browser: {url}")
-    sys.stdout.flush()
 
     client = receive_connection()
     data = client.recv(1024).decode("utf-8")
@@ -95,6 +69,28 @@ async def main():
     send_message(client, f"Refresh token: {refresh_token}")
     await reddit._http.close()
     return 0
+
+
+def receive_connection():
+    """Wait for and then return a connected socket..
+
+    Opens a TCP connection on port 8080, and waits for a single client.
+
+    """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("localhost", 8080))
+    server.listen(1)
+    client = server.accept()[0]
+    server.close()
+    return client
+
+
+def send_message(client, message):
+    """Send message to client and close the connection."""
+    print(message)
+    client.send(f"HTTP/1.1 200 OK\r\n\r\n{message}".encode("utf-8"))
+    client.close()
 
 
 if __name__ == "__main__":
