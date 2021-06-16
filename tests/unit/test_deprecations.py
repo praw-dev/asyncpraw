praw-dev/praw

Objectify user subreddit

tests/unit/test_deprecations.py
@@ -4,6 +4,7 @@ import pytest
 
 from praw import Reddit
 from praw.exceptions import APIException, WebSocketException
+from praw.models.reddit.user_subreddit import UserSubreddit
 
 from . import UnitTest
 
@@ -70,4 +71,19 @@ class TestDeprecation(UnitTest):
                 redirect_uri="dummy",
                 refresh_token="dummy",
                 user_agent="dummy",
+            )
+
+    def test_user_subreddit_as_dict(self):
+        user_subreddit = UserSubreddit(None, display_name="test")
+        with pytest.deprecated_call() as warning_info:
+            display_name = user_subreddit["display_name"]
+            assert display_name == "test"
+            assert (
+                warning_info.list[0].message.args[0]
+                == "`Redditor.subreddit` is no longer a dict and is now an `UserSubreddit` object. Accessing attributes using string indices is deprecated."
+            )
+            assert user_subreddit.keys() == user_subreddit.__dict__.keys()
+            assert (
+                warning_info.list[1].message.args[0]
+                == "`Redditor.subreddit` is no longer a dict and is now an `UserSubreddit` object. Using `keys` is deprecated and will be removed in PRAW 8."
             )

praw-dev/asyncpraw

(cherry picked from commit 28c75ce012dda7415f67bf8d6581c50e93ea3886)

tests/unit/test_deprecations.py
@@ -5,6 +5,7 @@ import pytest
 from asyncpraw import Reddit
 from asyncpraw.exceptions import APIException, AsyncPRAWException, WebSocketException
 from asyncpraw.models import Subreddit
+from asyncpraw.models.reddit.user_subreddit import UserSubreddit
 
 from . import UnitTest
 
@@ -99,4 +100,19 @@ class TestDeprecation(UnitTest):
                 redirect_uri="dummy",
                 refresh_token="dummy",
                 user_agent="dummy",
+            )
+
+    def test_user_subreddit_as_dict(self):
+        user_subreddit = UserSubreddit(None, display_name="test")
+        with pytest.deprecated_call() as warning_info:
+            display_name = user_subreddit["display_name"]
+            assert display_name == "test"
+            assert (
+                warning_info.list[0].message.args[0]
+                == "`Redditor.subreddit` is no longer a dict and is now an `UserSubreddit` object. Accessing attributes using string indices is deprecated."
+            )
+            assert user_subreddit.keys() == user_subreddit.__dict__.keys()
+            assert (
+                warning_info.list[1].message.args[0]
+                == "`Redditor.subreddit` is no longer a dict and is now an `UserSubreddit` object. Using `keys` is deprecated and will be removed in Async PRAW 8."
             )