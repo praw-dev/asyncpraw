praw-dev/praw

Add ability for more than one batch of ModmailConversations to be fetched

praw/objector.py
@@ -63,8 +63,44 @@ class Objector:
         :returns: An instance of :class:`~.RedditBase`.
 
         """
-        if {"conversation", "messages", "modActions"}.issubset(data):
+        if {"messages", "modActions"}.issubset(data) and {
+            "conversations",
+            "conversation",
+        }.intersection(data):
+            data.update(
+                data.pop("conversation")
+                if "conversation" in data
+                else data.pop("conversations")
+            )
+            parser: "praw.models.ModmailConversation" = self.parsers[
+                "ModmailConversation"
+            ]
+            parser._convert_conversation_objects(data, self._reddit)
+        elif {"messages", "modActions"}.issubset(data):
             parser = self.parsers["ModmailConversation"]
+        elif {"conversationIds", "conversations", "messages"}.issubset(data):
+            conversations = []
+            temp_data = {
+                thing_type: data.pop(thing_type, {})
+                for thing_type in ["messages", "modActions"]
+            }
+            for conversation_id in data["conversationIds"]:
+                conversation_data = {
+                    **data["conversations"][conversation_id],
+                    "messages": [],
+                    "modActions": [],
+                }
+                for object_id in conversation_data["objIds"]:
+                    conversation_data[object_id["key"]].append(
+                        self._objectify_dict(
+                            temp_data[object_id["key"]].pop(object_id["id"])
+                        )
+                    )
+                conversations.append(conversation_data)
+            data["conversations"] = conversations
+            del temp_data
+            data = snake_case_keys(data)
+            parser = self.parsers["ModmailConversations-list"]
         elif {"actionTypeId", "author", "date"}.issubset(data):
             # Modmail mod action
             data = snake_case_keys(data)

tests/integration/models/reddit/test_subreddit.py
@@ -1588,29 +1588,33 @@ class TestSubredditModmail(IntegrationTest):
     @mock.patch("time.sleep", return_value=None)
     def test_conversations(self, _):
         self.reddit.read_only = False
-        conversations = self.reddit.subreddit("all").modmail.conversations()
         with self.use_cassette():
+            conversations = self.reddit.subreddit("all").modmail.conversations()
             for conversation in conversations:
                 assert isinstance(conversation, ModmailConversation)
                 assert isinstance(conversation.authors[0], Redditor)
 
     @mock.patch("time.sleep", return_value=None)
-    def test_conversations__params(self, _):
-        self.reddit.read_only = False
-        conversations = self.reddit.subreddit("all").modmail.conversations(state="mod")
-        with self.use_cassette():
-            for conversation in conversations:
-                assert conversation.is_internal
-
-    @mock.patch("time.sleep", return_value=None)
     def test_conversations__other_subreddits(self, _):
         self.reddit.read_only = False
-        subreddit = self.reddit.subreddit("modmailtestA")
-        conversations = subreddit.modmail.conversations(
-            other_subreddits=["modmailtestB"]
-        )
         with self.use_cassette():
-            assert len(set(conversation.owner for conversation in conversations)) > 1
+            subreddit = self.reddit.subreddit("pics")
+            conversations = subreddit.modmail.conversations(
+                other_subreddits=["LifeProTips"]
+            )
+            assert (
+                len(set(list(conversation.owner for conversation in conversations))) > 1
+            )
+
+    @mock.patch("time.sleep", return_value=None)
+    def test_conversations__params(self, _):
+        self.reddit.read_only = False
+        with self.use_cassette():
+            conversations = self.reddit.subreddit("all").modmail.conversations(
+                state="mod"
+            )
+            for conversation in conversations:
+                assert conversation.is_internal
 
     @mock.patch("time.sleep", return_value=None)
     def test_create(self, _):
@@ -1890,7 +1894,7 @@ class TestSubredditModerationStreams(IntegrationTest):
         self.reddit.read_only = False
         with self.use_cassette():
             generator = self.reddit.subreddit("mod").mod.stream.modmail_conversations()
-            for i in range(10):
+            for i in range(101):
                 assert isinstance(next(generator), ModmailConversation)
 
     @mock.patch("time.sleep", return_value=None)

tests/unit/models/listing/test_listing.py
@@ -1,0 +1,11 @@
+from praw.models.listing.listing import ModmailConversationsListing
+
+from ... import UnitTest
+
+
+class TestModmailConversationsListing(UnitTest):
+    def test_empty_conversations_list(self):
+        assert (
+            ModmailConversationsListing(self.reddit, _data={"conversations": []}).after
+            is None
+        )

praw-dev/asyncpraw

(cherry picked from commit praw-dev/praw@18269a1f1dd874061bcdc564714c1d9ec7b5b348)

asyncpraw/objector.py
@@ -66,8 +66,44 @@ class Objector:
         :returns: An instance of :class:`~.RedditBase`.
 
         """
-        if {"conversation", "messages", "modActions"}.issubset(data):
+        if {"messages", "modActions"}.issubset(data) and {
+            "conversations",
+            "conversation",
+        }.intersection(data):
+            data.update(
+                data.pop("conversation")
+                if "conversation" in data
+                else data.pop("conversations")
+            )
+            parser: "asyncpraw.models.ModmailConversation" = self.parsers[
+                "ModmailConversation"
+            ]
+            parser._convert_conversation_objects(data, self._reddit)
+        elif {"messages", "modActions"}.issubset(data):
             parser = self.parsers["ModmailConversation"]
+        elif {"conversationIds", "conversations", "messages"}.issubset(data):
+            conversations = []
+            temp_data = {
+                thing_type: data.pop(thing_type, {})
+                for thing_type in ["messages", "modActions"]
+            }
+            for conversation_id in data["conversationIds"]:
+                conversation_data = {
+                    **data["conversations"][conversation_id],
+                    "messages": [],
+                    "modActions": [],
+                }
+                for object_id in conversation_data["objIds"]:
+                    conversation_data[object_id["key"]].append(
+                        self._objectify_dict(
+                            temp_data[object_id["key"]].pop(object_id["id"])
+                        )
+                    )
+                conversations.append(conversation_data)
+            data["conversations"] = conversations
+            del temp_data
+            data = snake_case_keys(data)
+            parser = self.parsers["ModmailConversations-list"]
         elif {"actionTypeId", "author", "date"}.issubset(data):
             # Modmail mod action
             data = snake_case_keys(data)

tests/integration/models/reddit/test_subreddit.py
@@ -1591,29 +1591,34 @@ class TestSubredditModmail(IntegrationTest):
     @mock.patch("asyncio.sleep", return_value=None)
     async def test_conversations(self, _):
         self.reddit.read_only = False
-        subreddit = await self.reddit.subreddit("all")
         with self.use_cassette():
-            async for conversation in subreddit.modmail.conversations():
+            subreddit = await self.reddit.subreddit("all")
+            conversations = subreddit.modmail.conversations()
+            async for conversation in conversations:
                 assert isinstance(conversation, ModmailConversation)
                 assert isinstance(conversation.authors[0], Redditor)
 
     @mock.patch("asyncio.sleep", return_value=None)
-    async def test_conversations__params(self, _):
-        self.reddit.read_only = False
-        subreddit = await self.reddit.subreddit("all")
-        with self.use_cassette():
-            async for conversation in subreddit.modmail.conversations(state="mod"):
-                assert conversation.is_internal
-
-    @mock.patch("asyncio.sleep", return_value=None)
     async def test_conversations__other_subreddits(self, _):
         self.reddit.read_only = False
-        subreddit = await self.reddit.subreddit(pytest.placeholders.test_subreddit)
         with self.use_cassette():
-            conversations = await self.async_list(
-                subreddit.modmail.conversations(other_subreddits=["dankmemes"])
+            subreddit = await self.reddit.subreddit("pics")
+            conversations = subreddit.modmail.conversations(
+                other_subreddits=["LifeProTips"]
             )
-            assert len(set(conversation.owner for conversation in conversations)) > 1
+            assert (
+                len(set([conversation.owner async for conversation in conversations]))
+                > 1
+            )
+
+    @mock.patch("asyncio.sleep", return_value=None)
+    async def test_conversations__params(self, _):
+        self.reddit.read_only = False
+        with self.use_cassette():
+            subreddit = await self.reddit.subreddit("all")
+            conversations = subreddit.modmail.conversations(state="mod")
+            async for conversation in conversations:
+                assert conversation.is_internal
 
     @mock.patch("asyncio.sleep", return_value=None)
     async def test_create(self, _):
@@ -1918,7 +1923,7 @@ class TestSubredditModerationStreams(IntegrationTest):
         subreddit = await self.reddit.subreddit("mod")
         with self.use_cassette():
             generator = subreddit.mod.stream.modmail_conversations()
-            for i in range(10):
+            for i in range(101):
                 assert isinstance(await self.async_next(generator), ModmailConversation)
 
     @mock.patch("asyncio.sleep", return_value=None)

tests/unit/models/listing/test_listing.py
@@ -1,0 +1,11 @@
+from asyncpraw.models.listing.listing import ModmailConversationsListing
+
+from ... import UnitTest
+
+
+class TestModmailConversationsListing(UnitTest):
+    def test_empty_conversations_list(self):
+        assert (
+            ModmailConversationsListing(self.reddit, _data={"conversations": []}).after
+            is None
+        )