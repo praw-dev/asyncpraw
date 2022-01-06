8fe1f854d78fdc253e35f40359c7c965a4ec6cfc
LilSpazJoekp <15524072+LilSpazJoekp@users.noreply.github.com>
2022-01-06 15:34:54 -0600

    Sort asyncpraw.models.reddit.collections.SubredditCollectionsModeration.create arguments
    (cherry picked from commit praw-dev/praw@5de11440a472ba48565a1a347114a17545e40250)

--- asyncpraw/models/reddit/collections.py
+++ asyncpraw/models/reddit/collections.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Any, Dict, Generator, List, Optional, Union
 
 from ...const import API_PATH
 from ...exceptions import ClientException
+from ...util import _deprecate_args
 from ...util.cache import cachedproperty
 from ..base import AsyncPRAWBase
 from ..util import deprecate_lazy
@@ -464,20 +465,21 @@ class SubredditCollectionsModeration(AsyncPRAWBase):
         super().__init__(reddit, _data)
         self.subreddit = subreddit
 
+    @_deprecate_args("title", "description", "display_layout")
     async def create(
-        self, title: str, description: str, display_layout: Optional[str] = None
+        self, *, description: str, display_layout: Optional[str] = None, title: str
     ):
         """Create a new :class:`.Collection`.
 
         The authenticated account must have appropriate moderator permissions in the
         subreddit this collection belongs to.
 
-        :param title: The title of the collection, up to 300 characters.
         :param description: The description, up to 500 characters.
         :param display_layout: Either ``"TIMELINE"`` for events or discussions or
             ``"GALLERY"`` for images or memes. Passing ``""`` or ``None`` will make the
             collection appear on Reddit as if this is set to ``"TIMELINE"`` (default:
             ``None``).
+        :param title: The title of the collection, up to 300 characters.
 
         :returns: The newly created :class:`.Collection`.
 
@@ -486,7 +488,7 @@ class SubredditCollectionsModeration(AsyncPRAWBase):
         .. code-block:: python
 
             sub = await reddit.subreddit("test")
-            new_collection = await sub.collections.mod.create("Title", "desc")
+            new_collection = await sub.collections.mod.create(title="Title", description="desc")
             await new_collection.mod.add_post("bgibu9")
 
         To specify the display layout as ``"GALLERY"`` when creating the collection:
@@ -540,7 +542,7 @@ class SubredditCollections(AsyncPRAWBase):
         .. code-block:: python
 
             my_sub = await reddit.subreddit("test", fetch=True)
-            new_collection = await my_sub.collections.mod.create("Title", "desc")
+            new_collection = await my_sub.collections.mod.create(title="Title", description="desc")
 
         """
         return SubredditCollectionsModeration(self._reddit, self.subreddit)
