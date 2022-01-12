b3061e91632767670a2fbcd1afd88c738bdf4e4c
LilSpazJoekp <15524072+LilSpazJoekp@users.noreply.github.com>
2022-01-12 16:14:20 -0600

    Sort asyncpraw.models.reddit.subreddit.SubredditModerationStream.log arguments
    (cherry picked from commit praw-dev/praw@d3cef5658decc4eb063b7a99f52cb64dcd19e16f)

--- asyncpraw/models/reddit/subreddit.py
+++ asyncpraw/models/reddit/subreddit.py
@@ -2860,8 +2860,10 @@ class SubredditModerationStream:
         """
         return stream_generator(self.subreddit.mod.edited, only=only, **stream_options)
 
+    @_deprecate_args("action", "mod")
     def log(
         self,
+        *,
         action: Optional[str] = None,
         mod: Optional[Union[str, "asyncpraw.models.Redditor"]] = None,
         **stream_options: Any,
