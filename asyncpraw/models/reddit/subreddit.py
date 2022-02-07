574192cab3e7e294500e7c6486ea5caba8583c48
LilSpazJoekp <15524072+LilSpazJoekp@users.noreply.github.com>
2022-02-06 18:26:22 -0600

    Sort asyncpraw.models.reddit.subreddit.SubredditModeration.edited arguments
    (cherry picked from commit praw-dev/praw@1f566eb5bc614f577e423318a2b6ce7d4ca243e4)

--- asyncpraw/models/reddit/subreddit.py
+++ asyncpraw/models/reddit/subreddit.py
@@ -2430,8 +2430,9 @@ class SubredditModeration:
         url = API_PATH["accept_mod_invite"].format(subreddit=self.subreddit)
         await self.subreddit._reddit.post(url)
 
+    @_deprecate_args("only")
     def edited(
-        self, only: Optional[str] = None, **generator_kwargs: Any
+        self, *, only: Optional[str] = None, **generator_kwargs: Any
     ) -> AsyncIterator[
         Union["asyncpraw.models.Comment", "asyncpraw.models.Submission"]
     ]:
