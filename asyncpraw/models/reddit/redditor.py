praw-dev/praw

Add support for mod notes
Co-authored-by: Joel Payne <15524072+LilSpazJoekp@users.noreply.github.com>

praw/models/reddit/redditor.py
@@ -81,6 +81,30 @@ class Redditor(MessageableMixin, RedditorListingMixin, FullnameMixin, RedditBase
         return cls(reddit, data)
 
     @cachedproperty
+    def notes(self) -> "praw.models.RedditorModNotes":
+        """Provide an instance of :class:`.RedditorModNotes`.
+
+        This provides an interface for managing moderator notes for a redditor.
+
+        .. note::
+
+            The authenticated user must be a moderator of the provided subreddit(s).
+
+        For example, all the notes for u/spez in r/test can be iterated through like so:
+
+        .. code-block:: python
+
+            redditor = reddit.redditor("spez")
+
+            for note in redditor.notes.subreddits("test"):
+                print(f"{note.label}: {note.note}")
+
+        """
+        from ..mod_notes import RedditorModNotes
+
+        return RedditorModNotes(self._reddit, self)
+
+    @cachedproperty
     def stream(self) -> "praw.models.reddit.redditor.RedditorStream":
         """Provide an instance of :class:`.RedditorStream`.
 

tests/unit/models/listing/test_listing.py
@@ -1,4 +1,4 @@
-from praw.models.listing.listing import ModmailConversationsListing
+from praw.models.listing.listing import ModmailConversationsListing, ModNoteListing
 
 from ... import UnitTest
 
@@ -8,4 +8,14 @@ class TestModmailConversationsListing(UnitTest):
         assert (
             ModmailConversationsListing(self.reddit, _data={"conversations": []}).after
             is None
+        )
+
+
+class TestModNoteListing(UnitTest):
+    def test_has_next_page(self):
+        assert (
+            ModNoteListing(
+                self.reddit, _data={"has_next_page": True, "end_cursor": "end_cursor"}
+            ).after
+            == "end_cursor"
         )

praw-dev/asyncpraw

(cherry picked from commit praw-dev/praw@b22e1f514d68d36545daf62e8a8d6c6c8caf782b)

asyncpraw/models/reddit/redditor.py
@@ -81,6 +81,30 @@ class Redditor(MessageableMixin, RedditorListingMixin, FullnameMixin, RedditBase
         return cls(reddit, data)
 
     @cachedproperty
+    def notes(self) -> "asyncpraw.models.RedditorModNotes":
+        """Provide an instance of :class:`.RedditorModNotes`.
+
+        This provides an interface for managing moderator notes for a redditor.
+
+        .. note::
+
+            The authenticated user must be a moderator of the provided subreddit(s).
+
+        For example, all the notes for u/spez in r/test can be iterated through like so:
+
+        .. code-block:: python
+
+            redditor = await reddit.redditor("spez")
+
+            async for note in redditor.notes.subreddits("test"):
+                print(f"{note.label}: {note.note}")
+
+        """
+        from ..mod_notes import RedditorModNotes
+
+        return RedditorModNotes(self._reddit, self)
+
+    @cachedproperty
     def stream(self) -> "asyncpraw.models.reddit.redditor.RedditorStream":
         """Provide an instance of :class:`.RedditorStream`.
 

tests/unit/models/listing/test_listing.py
@@ -1,4 +1,5 @@
-from asyncpraw.models.listing.listing import ModmailConversationsListing
+"""Test asyncpraw.models.listing.listing."""
+from asyncpraw.models.listing.listing import ModmailConversationsListing, ModNoteListing
 
 from ... import UnitTest
 
@@ -8,4 +9,14 @@ class TestModmailConversationsListing(UnitTest):
         assert (
             ModmailConversationsListing(reddit, _data={"conversations": []}).after
             is None
+        )
+
+
+class TestModNoteListing(UnitTest):
+    def test_has_next_page(self, reddit):
+        assert (
+            ModNoteListing(
+                reddit, _data={"has_next_page": True, "end_cursor": "end_cursor"}
+            ).after
+            == "end_cursor"
         )