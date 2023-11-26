praw-dev/praw

Fix type checking imports

praw/models/reddit/mixins/editable.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 from ....const import API_PATH
 
 if TYPE_CHECKING:  # pragma: no cover
-    import praw
+    import praw.models
 
 
 class EditableMixin:

praw/models/reddit/mixins/modnote.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, Generator
 
 if TYPE_CHECKING:  # pragma: no cover
-    import praw
+    import praw.models
 
 
 class ModNoteMixin:

praw/models/reddit/mixins/replyable.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 from ....const import API_PATH
 
 if TYPE_CHECKING:  # pragma: no cover
-    import praw
+    import praw.models
 
 
 class ReplyableMixin:

praw/models/reddit/rules.py
@@ -11,7 +11,7 @@ from ...util import _deprecate_args, cachedproperty
 from .base import RedditBase
 
 if TYPE_CHECKING:  # pragma: no cover
-    import praw
+    import praw.models
 
 
 class Rule(RedditBase):

praw/models/reddit/widgets.py
@@ -12,7 +12,7 @@ from ..base import PRAWBase
 from ..list.base import BaseList
 
 if TYPE_CHECKING:  # pragma: no cover
-    import praw
+    import praw.models
 
 WidgetType: TypeVar = TypeVar("WidgetType", bound="Widget")
 

praw/models/redditors.py
@@ -13,7 +13,7 @@ from .listing.generator import ListingGenerator
 from .util import stream_generator
 
 if TYPE_CHECKING:  # pragma: no cover
-    import praw
+    import praw.models
 
 
 class PartialRedditor(SimpleNamespace):

praw/models/subreddits.py
@@ -12,7 +12,7 @@ from .listing.generator import ListingGenerator
 from .util import stream_generator
 
 if TYPE_CHECKING:  # pragma: no cover
-    import praw
+    import praw.models
 
 
 class Subreddits(PRAWBase):

praw/models/user.py
@@ -17,7 +17,7 @@ from .reddit.redditor import Redditor
 from .reddit.subreddit import Subreddit
 
 if TYPE_CHECKING:  # pragma: no cover
-    import praw
+    import praw.models
 
 
 class User(PRAWBase):

praw-dev/asyncpraw

(cherry picked from commit praw-dev/praw@ff019f3e3144ca815330036d8c6254778c1a06b5)

asyncpraw/models/reddit/mixins/editable.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 from ....const import API_PATH
 
 if TYPE_CHECKING:  # pragma: no cover
-    import asyncpraw
+    import asyncpraw.models
 
 
 class EditableMixin:

asyncpraw/models/reddit/mixins/modnote.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, AsyncGenerator
 
 if TYPE_CHECKING:  # pragma: no cover
-    import asyncpraw
+    import asyncpraw.models
 
 
 class ModNoteMixin:

asyncpraw/models/reddit/mixins/replyable.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 from ....const import API_PATH
 
 if TYPE_CHECKING:  # pragma: no cover
-    import asyncpraw
+    import asyncpraw.models
 
 
 class ReplyableMixin:

asyncpraw/models/reddit/rules.py
@@ -11,7 +11,7 @@ from ...util import _deprecate_args, cachedproperty
 from .base import RedditBase
 
 if TYPE_CHECKING:  # pragma: no cover
-    import asyncpraw
+    import asyncpraw.models
 
 
 class Rule(RedditBase):

asyncpraw/models/reddit/widgets.py
@@ -12,7 +12,7 @@ from ..base import AsyncPRAWBase
 from ..list.base import BaseList
 
 if TYPE_CHECKING:  # pragma: no cover
-    import asyncpraw
+    import asyncpraw.models
 
 WidgetType: TypeVar = TypeVar("WidgetType", bound="Widget")
 

asyncpraw/models/redditors.py
@@ -13,7 +13,7 @@ from .listing.generator import ListingGenerator
 from .util import stream_generator
 
 if TYPE_CHECKING:  # pragma: no cover
-    import asyncpraw
+    import asyncpraw.models
 
 
 class PartialRedditor(SimpleNamespace):

asyncpraw/models/subreddits.py
@@ -12,7 +12,7 @@ from .listing.generator import ListingGenerator
 from .util import stream_generator
 
 if TYPE_CHECKING:  # pragma: no cover
-    import asyncpraw
+    import asyncpraw.models
 
 
 class Subreddits(AsyncPRAWBase):

asyncpraw/models/user.py
@@ -17,7 +17,7 @@ from .reddit.redditor import Redditor
 from .reddit.subreddit import Subreddit
 
 if TYPE_CHECKING:  # pragma: no cover
-    import asyncpraw
+    import asyncpraw.models
 
 
 class User(AsyncPRAWBase):