praw-dev/praw

Update linting rules for project

praw/models/front.py
@@ -2,13 +2,15 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Iterator
+from typing import TYPE_CHECKING
 from urllib.parse import urljoin
 
 from .listing.generator import ListingGenerator
 from .listing.mixins import SubredditListingMixin
 
 if TYPE_CHECKING:  # pragma: no cover
+    from collections.abc import Iterator
+
     import praw.models
 
 

praw/models/listing/generator.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 
+from collections.abc import Iterator
 from copy import deepcopy
-from typing import TYPE_CHECKING, Any, Iterator
+from typing import TYPE_CHECKING, Any
 
 from ..base import PRAWBase
 from .listing import FlairListing, ModNoteListing

praw/models/reddit/mixins/modnote.py
@@ -2,9 +2,11 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Generator
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:  # pragma: no cover
+    from collections.abc import Generator
+
     import praw.models
 
 

praw/models/reddit/rules.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Iterator
+from typing import TYPE_CHECKING, Any
 from urllib.parse import quote
 from warnings import warn
 
@@ -12,6 +12,8 @@ from ...util import _deprecate_args, cachedproperty
 from .base import RedditBase
 
 if TYPE_CHECKING:  # pragma: no cover
+    from collections.abc import Iterator
+
     import praw.models
 
 

praw/reddit.py
@@ -9,7 +9,7 @@ import re
 import time
 from itertools import islice
 from logging import getLogger
-from typing import IO, TYPE_CHECKING, Any, Generator, Iterable
+from typing import IO, TYPE_CHECKING, Any
 from urllib.parse import urlparse
 from warnings import warn
 
@@ -46,6 +46,8 @@ except ImportError:  # pragma: no cover
     UPDATE_CHECKER_MISSING = True
 
 if TYPE_CHECKING:  # pragma: no cover
+    from collections.abc import Generator, Iterable
+
     import prawcore
 
     import praw.models

praw-dev/asyncpraw

(cherry picked from commit praw-dev/praw@64485a095b46a9732ee07cbe012a5391c95e092b)

asyncpraw/models/front.py
@@ -2,13 +2,15 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, AsyncIterator
+from typing import TYPE_CHECKING
 from urllib.parse import urljoin
 
 from .listing.generator import ListingGenerator
 from .listing.mixins import SubredditListingMixin
 
 if TYPE_CHECKING:  # pragma: no cover
+    from collections.abc import AsyncIterator
+
     import asyncpraw.models
 
 

asyncpraw/models/listing/generator.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 
+from collections.abc import AsyncIterator
 from copy import deepcopy
-from typing import TYPE_CHECKING, Any, AsyncIterator
+from typing import TYPE_CHECKING, Any
 
 from ..base import AsyncPRAWBase
 from .listing import FlairListing, ModNoteListing

asyncpraw/models/reddit/mixins/modnote.py
@@ -2,9 +2,11 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, AsyncGenerator
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:  # pragma: no cover
+    from collections.abc import AsyncGenerator
+
     import asyncpraw.models
 
 

asyncpraw/models/reddit/rules.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, AsyncIterator
+from typing import TYPE_CHECKING, Any
 from urllib.parse import quote
 from warnings import warn
 
@@ -12,6 +12,8 @@ from ...util import _deprecate_args, cachedproperty
 from .base import RedditBase
 
 if TYPE_CHECKING:  # pragma: no cover
+    from collections.abc import AsyncIterator
+
     import asyncpraw.models
 
 

asyncpraw/reddit.py
@@ -9,13 +9,7 @@ import re
 from copy import copy
 from itertools import islice
 from logging import getLogger
-from typing import (
-    IO,
-    TYPE_CHECKING,
-    Any,
-    AsyncGenerator,
-    Iterable,
-)
+from typing import IO, TYPE_CHECKING, Any
 from urllib.parse import urlparse
 from warnings import warn
 
@@ -52,6 +46,8 @@ except ImportError:  # pragma: no cover
     UPDATE_CHECKER_MISSING = True
 
 if TYPE_CHECKING:  # pragma: no cover
+    from collections.abc import AsyncGenerator, Iterable
+
     import asyncprawcore
 
     import asyncpraw