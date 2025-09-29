praw-dev/praw

Remove APIException class

praw/exceptions.py
@@ -10,7 +10,6 @@ All other exceptions are subclassed from :class:`.ClientException`.
 
 from __future__ import annotations
 
-from typing import Any
 from warnings import warn
 
 from .util import _deprecate_args
@@ -202,15 +201,8 @@ class MediaPostFailed(WebSocketException):
         )
 
 
-class APIException(PRAWException):
-    """Old class preserved for alias purposes.
-
-    .. deprecated:: 7.0
-
-        Class :class:`.APIException` has been deprecated in favor of
-        :class:`.RedditAPIException`. This class will be removed in PRAW 8.0.
-
-    """
+class RedditAPIException(PRAWException):
+    """Container for error messages from Reddit's API."""
 
     @staticmethod
     def parse_exception_list(
@@ -230,79 +222,17 @@ class APIException(PRAWException):
             for exception in exceptions
         ]
 
-    @property
-    def error_type(self) -> str:
-        """Get error_type.
-
-        .. deprecated:: 7.0
-
-            Accessing attributes through instances of :class:`.RedditAPIException` is
-            deprecated. This behavior will be removed in PRAW 8.0. Check out the
-            :ref:`PRAW 7 Migration tutorial <Exception_Handling>` on how to migrate code
-            from this behavior.
-
-        """
-        return self._get_old_attr("error_type")
-
-    @property
-    def field(self) -> str:
-        """Get field.
-
-        .. deprecated:: 7.0
-
-            Accessing attributes through instances of :class:`.RedditAPIException` is
-            deprecated. This behavior will be removed in PRAW 8.0. Check out the
-            :ref:`PRAW 7 Migration tutorial <Exception_Handling>` on how to migrate code
-            from this behavior.
-
-        """
-        return self._get_old_attr("field")
-
-    @property
-    def message(self) -> str:
-        """Get message.
-
-        .. deprecated:: 7.0
-
-            Accessing attributes through instances of :class:`.RedditAPIException` is
-            deprecated. This behavior will be removed in PRAW 8.0. Check out the
-            :ref:`PRAW 7 Migration tutorial <Exception_Handling>` on how to migrate code
-            from this behavior.
-
-        """
-        return self._get_old_attr("message")
-
     def __init__(
         self,
-        items: list[RedditErrorItem | list[str] | str] | str,
-        *optional_args: str,
+        items: list[RedditErrorItem | list[str] | str]
     ):
         """Initialize a :class:`.RedditAPIException` instance.
 
         :param items: Either a list of instances of :class:`.RedditErrorItem` or a list
             containing lists of unformed errors.
-        :param optional_args: Takes the second and third arguments that
-            :class:`.APIException` used to take.
 
         """
-        if isinstance(items, str):
-            items = [[items, *optional_args]]
-        elif isinstance(items, list) and isinstance(items[0], str):
+        if isinstance(items, list) and isinstance(items[0], str):
             items = [items]
         self.items = self.parse_exception_list(items)
         super().__init__(*self.items)
-
-    def _get_old_attr(self, attrname: str) -> Any:
-        warn(
-            f"Accessing attribute '{attrname}' through APIException is deprecated."
-            " This behavior will be removed in PRAW 8.0. Check out"
-            " https://praw.readthedocs.io/en/latest/package_info/praw7_migration.html"
-            " to learn how to migrate your code.",
-            category=DeprecationWarning,
-            stacklevel=3,
-        )
-        return getattr(self.items[0], attrname)
-
-
-class RedditAPIException(APIException):
-    """Container for error messages from Reddit's API."""

tests/unit/test_deprecations.py
@@ -3,7 +3,7 @@
 import pytest
 
 from praw import Reddit
-from praw.exceptions import APIException, WebSocketException
+from praw.exceptions import WebSocketException
 from praw.models.reddit.user_subreddit import UserSubreddit
 from praw.util.token_manager import FileTokenManager
 
@@ -12,15 +12,6 @@ from . import UnitTest
 
 @pytest.mark.filterwarnings("error", category=DeprecationWarning)
 class TestDeprecation(UnitTest):
-    def test_api_exception(self):
-        exc = APIException(["test", "testing", "test"])
-        with pytest.raises(DeprecationWarning):
-            exc.error_type
-        with pytest.raises(DeprecationWarning):
-            exc.message
-        with pytest.raises(DeprecationWarning):
-            exc.field
-
     def test_conversations_after_argument(self, reddit):
         with pytest.deprecated_call():
             reddit.subreddit("all").modmail.conversations(after="after")

praw-dev/asyncpraw

(cherry picked from commit praw-dev/praw@6b9e8edd38ae999aebec2f7e1bc41338c92f2497)

asyncpraw/exceptions.py
@@ -10,8 +10,6 @@ All other exceptions are subclassed from :class:`.ClientException`.
 
 from __future__ import annotations
 
-import sys
-from typing import Any
 from warnings import warn
 
 from .util import _deprecate_args
@@ -19,29 +17,6 @@ from .util import _deprecate_args
 
 class AsyncPRAWException(Exception):
     """The base Async PRAW Exception that all other exception classes extend."""
-
-
-PRAWException = AsyncPRAWException
-
-
-# Adapted from https://stackoverflow.com/a/40546615
-class ExceptionWrapper:
-    """Wrapper to facilitate showing depreciation for PRAWException class rename."""
-
-    def __getattr__(self, attribute: str) -> Any:
-        """Return the value of `attribute`."""
-        if attribute == "PRAWException":
-            warn(
-                "PRAWException as been renamed to AsyncPRAWException. PRAWException "
-                " will be removed in the next version Async PRAW.",
-                category=DeprecationWarning,
-                stacklevel=3,
-            )
-        return getattr(self.wrapped, attribute)
-
-    def __init__(self, wrapped: Any):
-        """Initialize Wrapper instance."""
-        self.wrapped = wrapped
 
 
 class RedditErrorItem:
@@ -226,15 +201,8 @@ class MediaPostFailed(WebSocketException):
         )
 
 
-class APIException(AsyncPRAWException):
-    """Old class preserved for alias purposes.
-
-    .. deprecated:: 7.0
-
-        Class :class:`.APIException` has been deprecated in favor of
-        :class:`.RedditAPIException`. This class will be removed in Async PRAW 8.0.
-
-    """
+class RedditAPIException(AsyncPRAWException):
+    """Container for error messages from Reddit's API."""
 
     @staticmethod
     def parse_exception_list(
@@ -254,83 +222,17 @@ class APIException(AsyncPRAWException):
             for exception in exceptions
         ]
 
-    @property
-    def error_type(self) -> str:
-        """Get error_type.
-
-        .. deprecated:: 7.0
-
-            Accessing attributes through instances of :class:`.RedditAPIException` is
-            deprecated. This behavior will be removed in Async PRAW 8.0. Check out the
-            :ref:`PRAW 7 Migration tutorial <Exception_Handling>` on how to migrate code
-            from this behavior.
-
-        """
-        return self._get_old_attr("error_type")
-
-    @property
-    def field(self) -> str:
-        """Get field.
-
-        .. deprecated:: 7.0
-
-            Accessing attributes through instances of :class:`.RedditAPIException` is
-            deprecated. This behavior will be removed in Async PRAW 8.0. Check out the
-            :ref:`PRAW 7 Migration tutorial <Exception_Handling>` on how to migrate code
-            from this behavior.
-
-        """
-        return self._get_old_attr("field")
-
-    @property
-    def message(self) -> str:
-        """Get message.
-
-        .. deprecated:: 7.0
-
-            Accessing attributes through instances of :class:`.RedditAPIException` is
-            deprecated. This behavior will be removed in Async PRAW 8.0. Check out the
-            :ref:`Async PRAW 7 Migration tutorial <Exception_Handling>` on how to
-            migrate code from this behavior.
-
-        """
-        return self._get_old_attr("message")
-
     def __init__(
         self,
-        items: list[RedditErrorItem | list[str] | str] | str,
-        *optional_args: str,
+        items: list[RedditErrorItem | list[str] | str]
     ):
         """Initialize a :class:`.RedditAPIException` instance.
 
         :param items: Either a list of instances of :class:`.RedditErrorItem` or a list
             containing lists of unformed errors.
-        :param optional_args: Takes the second and third arguments that
-            :class:`.APIException` used to take.
 
         """
-        if isinstance(items, str):
-            items = [[items, *optional_args]]
-        elif isinstance(items, list) and isinstance(items[0], str):
+        if isinstance(items, list) and isinstance(items[0], str):
             items = [items]
         self.items = self.parse_exception_list(items)
         super().__init__(*self.items)
-
-    def _get_old_attr(self, attrname: str) -> Any:
-        warn(
-            f"Accessing attribute '{attrname}' through APIException is deprecated."
-            " This behavior will be removed in Async PRAW 8.0. Check out"
-            " https://praw.readthedocs.io/en/latest/package_info/praw7_migration.html"
-            " to learn how to migrate your code.",
-            category=DeprecationWarning,
-            stacklevel=3,
-        )
-        return getattr(self.items[0], attrname)
-
-
-class RedditAPIException(APIException):
-    """Container for error messages from Reddit's API."""
-
-
-if "sphinx" not in sys.modules:
-    sys.modules[__name__] = ExceptionWrapper(sys.modules[__name__])

tests/unit/test_deprecations.py
@@ -3,8 +3,8 @@
 import pytest
 
 from asyncpraw import Reddit
-from asyncpraw.exceptions import APIException, AsyncPRAWException, WebSocketException
 from asyncpraw.models import Comment, Subreddit
+from asyncpraw.exceptions import WebSocketException
 from asyncpraw.models.reddit.user_subreddit import UserSubreddit
 from asyncpraw.util.token_manager import FileTokenManager
 
@@ -13,15 +13,6 @@ from . import UnitTest
 
 @pytest.mark.filterwarnings("error", category=DeprecationWarning)
 class TestDeprecation(UnitTest):
-    def test_api_exception(self):
-        exc = APIException(["test", "testing", "test"])
-        with pytest.deprecated_call():
-            exc.error_type
-        with pytest.deprecated_call():
-            exc.message
-        with pytest.deprecated_call():
-            exc.field
-
     async def test_comment_forest_async_iterator(self, reddit):
         submission = await reddit.submission("1234", fetch=False)
         submission._fetched = True
@@ -67,23 +58,6 @@ class TestDeprecation(UnitTest):
             == "The parameter ``lazy`` has been renamed to ``fetch`` and support for"
             " the ``lazy`` parameter will be removed in a future version of Async PRAW."
         )
-
-    def test_praw_exception_rename(self):
-        with pytest.raises(AsyncPRAWException):
-            Reddit()
-
-        with pytest.deprecated_call():
-            import asyncpraw
-
-            asyncpraw.exceptions.PRAWException
-
-        with pytest.deprecated_call():
-            from asyncpraw import exceptions
-
-            exceptions.PRAWException
-
-        with pytest.deprecated_call():
-            from asyncpraw.exceptions import PRAWException  # noqa: F401
 
     async def test_reddit_token_manager(self):
         with pytest.deprecated_call():