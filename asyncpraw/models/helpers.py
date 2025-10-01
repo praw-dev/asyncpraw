praw-dev/praw

Replace uses of Generator[T, None, None] with Iterator[T]

praw/models/helpers.py
@@ -12,7 +12,7 @@ from praw.models.reddit.live import LiveThread
 from praw.models.reddit.multi import Multireddit, Subreddit
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
+    from collections.abc import Iterator
 
     import praw.models
 
@@ -180,7 +180,7 @@ class LiveHelper(PRAWBase):
             },
         )
 
-    def info(self, ids: list[str]) -> Generator[praw.models.LiveThread, None, None]:
+    def info(self, ids: list[str]) -> Iterator[praw.models.LiveThread]:
         """Fetch information about each live thread in ``ids``.
 
         :param ids: A list of IDs for a live thread.
@@ -213,7 +213,7 @@ class LiveHelper(PRAWBase):
             msg = "ids must be a list"
             raise TypeError(msg)
 
-        def generator() -> Generator[praw.models.LiveThread, None, None]:
+        def generator() -> Iterator[praw.models.LiveThread]:
             for position in range(0, len(ids), 100):
                 ids_chunk = ids[position : position + 100]
                 url = API_PATH["live_info"].format(ids=",".join(ids_chunk))

praw/models/reddit/mixins/modnote.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
+    from collections.abc import Iterator
 
     import praw.models
 
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 class ModNoteMixin:
     """Interface for classes that can have a moderator note set on them."""
 
-    def author_notes(self, **generator_kwargs: Any) -> Generator[praw.models.ModNote, None, None]:
+    def author_notes(self, **generator_kwargs: Any) -> Iterator[praw.models.ModNote]:
         """Get the moderator notes for the author of this object in the subreddit it's posted in.
 
         :param generator_kwargs: Additional keyword arguments are passed in the

praw/models/reddit/redditor.py
@@ -13,7 +13,7 @@ from praw.models.util import stream_generator
 from praw.util.cache import cachedproperty
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
+    from collections.abc import Iterator
 
     import praw.models
 
@@ -420,7 +420,7 @@ class RedditorStream:
         """
         self.redditor = redditor
 
-    def comments(self, **stream_options: str | int | dict[str, str]) -> Generator[praw.models.Comment, None, None]:
+    def comments(self, **stream_options: str | int | dict[str, str]) -> Iterator[praw.models.Comment]:
         """Yield new comments as they become available.
 
         Comments are yielded oldest first. Up to 100 historical comments will initially
@@ -438,9 +438,7 @@ class RedditorStream:
         """
         return stream_generator(self.redditor.comments.new, **stream_options)
 
-    def submissions(
-        self, **stream_options: str | int | dict[str, str]
-    ) -> Generator[praw.models.Submission, None, None]:
+    def submissions(self, **stream_options: str | int | dict[str, str]) -> Iterator[praw.models.Submission]:
         """Yield new submissions as they become available.
 
         Submissions are yielded oldest first. Up to 100 historical submissions will

praw/models/reddit/subreddit.py
@@ -224,7 +224,7 @@ class Modmail:
         }
         return self.subreddit._reddit.post(API_PATH["modmail_conversations"], data=data)
 
-    def subreddits(self) -> Generator[praw.models.Subreddit, None, None]:
+    def subreddits(self) -> Iterator[praw.models.Subreddit]:
         """Yield subreddits using the new modmail that the user moderates.
 
         For example:
@@ -284,7 +284,7 @@ class SubredditFilters:
         """
         self.subreddit = subreddit
 
-    def __iter__(self) -> Generator[praw.models.Subreddit, None, None]:
+    def __iter__(self) -> Iterator[praw.models.Subreddit]:
         """Iterate through the special :class:`.Subreddit`'s filters.
 
         This method should be invoked as:
@@ -597,7 +597,7 @@ class SubredditFlairTemplates:
         """
         self.subreddit = subreddit
 
-    def __iter__(self) -> Generator[None, None, None]:
+    def __iter__(self) -> Iterator[None]:
         """Abstract method to return flair templates."""
         raise NotImplementedError
 
@@ -1112,7 +1112,7 @@ class SubredditModerationStream:
 
     def edited(
         self, *, only: str | None = None, **stream_options: Any
-    ) -> Generator[praw.models.Comment | praw.models.Submission, None, None]:
+    ) -> Iterator[praw.models.Comment | praw.models.Submission]:
         """Yield edited comments and submissions as they become available.
 
         :param only: If specified, one of ``"comments"`` or ``"submissions"`` to yield
@@ -1137,7 +1137,7 @@ class SubredditModerationStream:
         action: str | None = None,
         mod: str | praw.models.Redditor | None = None,
         **stream_options: Any,
-    ) -> Generator[praw.models.ModAction, None, None]:
+    ) -> Iterator[praw.models.ModAction]:
         """Yield moderator log entries as they become available.
 
         :param action: If given, only return log entries for the specified action.
@@ -1168,7 +1168,7 @@ class SubredditModerationStream:
         sort: str | None = None,
         state: str | None = None,
         **stream_options: Any,
-    ) -> Generator[ModmailConversation, None, None]:
+    ) -> Iterator[ModmailConversation]:
         """Yield new-modmail conversations as they become available.
 
         :param other_subreddits: A list of :class:`.Subreddit` instances for which to
@@ -1206,7 +1206,7 @@ class SubredditModerationStream:
 
     def modqueue(
         self, *, only: str | None = None, **stream_options: Any
-    ) -> Generator[praw.models.Comment | praw.models.Submission, None, None]:
+    ) -> Iterator[praw.models.Comment | praw.models.Submission]:
         r"""Yield :class:`.Comment`\ s and :class:`.Submission`\ s in the modqueue as they become available.
 
         :param only: If specified, one of ``"comments"`` or ``"submissions"`` to yield
@@ -1226,7 +1226,7 @@ class SubredditModerationStream:
 
     def reports(
         self, *, only: str | None = None, **stream_options: Any
-    ) -> Generator[praw.models.Comment | praw.models.Submission, None, None]:
+    ) -> Iterator[praw.models.Comment | praw.models.Submission]:
         r"""Yield reported :class:`.Comment`\ s and :class:`.Submission`\ s as they become available.
 
         :param only: If specified, one of ``"comments"`` or ``"submissions"`` to yield
@@ -1246,7 +1246,7 @@ class SubredditModerationStream:
 
     def spam(
         self, *, only: str | None = None, **stream_options: Any
-    ) -> Generator[praw.models.Comment | praw.models.Submission, None, None]:
+    ) -> Iterator[praw.models.Comment | praw.models.Submission]:
         r"""Yield spam :class:`.Comment`\ s and :class:`.Submission`\ s as they become available.
 
         :param only: If specified, one of ``"comments"`` or ``"submissions"`` to yield
@@ -1264,7 +1264,7 @@ class SubredditModerationStream:
         """
         return stream_generator(self.subreddit.mod.spam, only=only, **stream_options)
 
-    def unmoderated(self, **stream_options: Any) -> Generator[praw.models.Submission, None, None]:
+    def unmoderated(self, **stream_options: Any) -> Iterator[praw.models.Submission]:
         r"""Yield unmoderated :class:`.Submission`\ s as they become available.
 
         Keyword arguments are passed to :func:`.stream_generator`.
@@ -1413,7 +1413,7 @@ class SubredditStream:
         """
         self.subreddit = subreddit
 
-    def comments(self, **stream_options: Any) -> Generator[praw.models.Comment, None, None]:
+    def comments(self, **stream_options: Any) -> Iterator[praw.models.Comment]:
         """Yield new comments as they become available.
 
         Comments are yielded oldest first. Up to 100 historical comments will initially
@@ -1445,7 +1445,7 @@ class SubredditStream:
         """
         return stream_generator(self.subreddit.comments, **stream_options)
 
-    def submissions(self, **stream_options: Any) -> Generator[praw.models.Submission, None, None]:
+    def submissions(self, **stream_options: Any) -> Iterator[praw.models.Submission]:
         r"""Yield new :class:`.Submission`\ s as they become available.
 
         Submissions are yielded oldest first. Up to 100 historical submissions will
@@ -1925,7 +1925,7 @@ class SubredditWiki:
         self.contributor = SubredditRelationship(subreddit, "wikicontributor")
         self.subreddit = subreddit
 
-    def __iter__(self) -> Generator[WikiPage, None, None]:
+    def __iter__(self) -> Iterator[WikiPage]:
         """Iterate through the pages of the wiki.
 
         This method is to be used to discover all wikipages for a subreddit:
@@ -3661,7 +3661,7 @@ class SubredditLinkFlairTemplates(SubredditFlairTemplates):
 
     def __iter__(
         self,
-    ) -> Generator[dict[str, str | int | bool | list[dict[str, str]]], None, None]:
+    ) -> Iterator[dict[str, str | int | bool | list[dict[str, str]]]]:
         """Iterate through the link flair templates as a moderator.
 
         For example:
@@ -3758,7 +3758,7 @@ class SubredditLinkFlairTemplates(SubredditFlairTemplates):
 
     def user_selectable(
         self,
-    ) -> Generator[dict[str, str | bool], None, None]:
+    ) -> Iterator[dict[str, str | bool]]:
         """Iterate through the link flair templates as a regular user.
 
         For example:
@@ -3778,7 +3778,7 @@ class SubredditRedditorFlairTemplates(SubredditFlairTemplates):
 
     def __iter__(
         self,
-    ) -> Generator[dict[str, str | int | bool | list[dict[str, str]]], None, None]:
+    ) -> Iterator[dict[str, str | int | bool | list[dict[str, str]]]]:
         """Iterate through the user flair templates.
 
         For example:

praw/models/reddit/wikipage.py
@@ -11,7 +11,7 @@ from praw.models.reddit.redditor import Redditor
 from praw.util.cache import cachedproperty
 
 if TYPE_CHECKING:
-    from collections.abc import Generator, Iterator
+    from collections.abc import Iterator
 
     import praw.models
 
@@ -174,7 +174,7 @@ class WikiPage(RedditBase):
         generator_kwargs: dict[str, Any],
         subreddit: praw.models.Subreddit,
         url: str,
-    ) -> Generator[dict[str, Redditor | WikiPage | str | int | bool | None], None, None]:
+    ) -> Iterator[dict[str, Redditor | WikiPage | str | int | bool | None]]:
         for revision in ListingGenerator(subreddit._reddit, url, **generator_kwargs):
             if revision["author"] is not None:
                 revision["author"] = Redditor(subreddit._reddit, _data=revision["author"]["data"])
@@ -289,7 +289,7 @@ class WikiPage(RedditBase):
         """
         return WikiPage(self.subreddit._reddit, self.subreddit, self.name, revision)
 
-    def revisions(self, **generator_kwargs: str | int | dict[str, str]) -> Generator[WikiPage, None, None]:
+    def revisions(self, **generator_kwargs: str | int | dict[str, str]) -> Iterator[WikiPage]:
         """Return a :class:`.ListingGenerator` for page revisions.
 
         Additional keyword arguments are passed in the initialization of

praw/models/util.py
@@ -8,7 +8,7 @@ from collections import OrderedDict
 from typing import TYPE_CHECKING, Any, Callable
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
+    from collections.abc import Iterator
 
 
 def permissions_string(*, known_permissions: set[str], permissions: list[str] | None) -> str:
@@ -42,7 +42,7 @@ def stream_generator(
     pause_after: int | None = None,
     skip_existing: bool = False,
     **function_kwargs: Any,
-) -> Generator[Any, None, None]:
+) -> Iterator[Any]:
     """Yield new items from ``function`` as they become available.
 
     :param function: A callable that returns a :class:`.ListingGenerator`, e.g.,

praw-dev/asyncpraw

(cherry picked from commit praw-dev/praw@5cd8031a77c7ffa93a92187ec77a29719509813a)

asyncpraw/models/helpers.py
@@ -12,7 +12,7 @@ from asyncpraw.models.reddit.live import LiveThread
 from asyncpraw.models.reddit.multi import Multireddit, Subreddit
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator
+    from collections.abc import AsyncIterator
 
     import asyncpraw.models
 
@@ -27,7 +27,7 @@ class DraftHelper(AsyncPRAWBase):
 
     """
 
-    async def __aiter__(self) -> AsyncGenerator[asyncpraw.models.Draft, None]:
+    async def __aiter__(self) -> AsyncIterator[asyncpraw.models.Draft]:
         r"""Iterate through all the :class:`.Draft`\ s.
 
         :returns: An asynchronous iterator containing all the currently authenticated
@@ -211,7 +211,7 @@ class LiveHelper(AsyncPRAWBase):
             },
         )
 
-    def info(self, ids: list[str]) -> AsyncGenerator[asyncpraw.models.LiveThread, None]:
+    def info(self, ids: list[str]) -> AsyncIterator[asyncpraw.models.LiveThread]:
         """Fetch information about each live thread in ``ids``.
 
         :param ids: A list of IDs for a live thread.
@@ -244,7 +244,7 @@ class LiveHelper(AsyncPRAWBase):
             msg = "ids must be a list"
             raise TypeError(msg)
 
-        async def generator() -> AsyncGenerator[asyncpraw.models.LiveThread, None, None]:
+        async def generator() -> AsyncIterator[asyncpraw.models.LiveThread]:
             for position in range(0, len(ids), 100):
                 ids_chunk = ids[position : position + 100]
                 url = API_PATH["live_info"].format(ids=",".join(ids_chunk))

asyncpraw/models/reddit/mixins/modnote.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator
+    from collections.abc import AsyncIterator
 
     import asyncpraw.models
 
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 class ModNoteMixin:
     """Interface for classes that can have a moderator note set on them."""
 
-    def author_notes(self, **generator_kwargs: Any) -> AsyncGenerator[asyncpraw.models.ModNote, None]:
+    def author_notes(self, **generator_kwargs: Any) -> AsyncIterator[asyncpraw.models.ModNote]:
         """Get the moderator notes for the author of this object in the subreddit it's posted in.
 
         :param generator_kwargs: Additional keyword arguments are passed in the

asyncpraw/models/reddit/redditor.py
@@ -13,7 +13,7 @@ from asyncpraw.models.util import stream_generator
 from asyncpraw.util.cache import cachedproperty
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator
+    from collections.abc import AsyncIterator
 
     import asyncpraw.models
 
@@ -437,7 +437,7 @@ class RedditorStream:
         """
         self.redditor = redditor
 
-    def comments(self, **stream_options: str | int | dict[str, str]) -> AsyncGenerator[asyncpraw.models.Comment, None]:
+    def comments(self, **stream_options: str | int | dict[str, str]) -> AsyncIterator[asyncpraw.models.Comment]:
         """Yield new comments as they become available.
 
         Comments are yielded oldest first. Up to 100 historical comments will initially
@@ -458,7 +458,7 @@ class RedditorStream:
 
     def submissions(
         self, **stream_options: str | int | dict[str, str]
-    ) -> AsyncGenerator[asyncpraw.models.Submission, None]:
+    ) -> AsyncIterator[asyncpraw.models.Submission]:
         """Yield new submissions as they become available.
 
         Submissions are yielded oldest first. Up to 100 historical submissions will

asyncpraw/models/reddit/subreddit.py
@@ -251,7 +251,7 @@ class Modmail:
 
     async def subreddits(
         self,
-    ) -> AsyncGenerator[asyncpraw.models.Subreddit, None]:
+    ) -> AsyncGenerator[asyncpraw.models.Subreddit]:
         """Yield subreddits using the new modmail that the user moderates.
 
         For example:
@@ -306,7 +306,7 @@ class SubredditFilters:
 
     async def __aiter__(
         self,
-    ) -> AsyncGenerator[asyncpraw.models.Subreddit, None]:
+    ) -> AsyncIterator[asyncpraw.models.Subreddit]:
         """Iterate through the special :class:`.Subreddit`'s filters.
 
         This method should be invoked as:
@@ -625,7 +625,7 @@ class SubredditFlairTemplates:
         """Return ``"LINK_FLAIR"`` or ``"USER_FLAIR"`` depending on ``is_link`` value."""
         return "LINK_FLAIR" if is_link else "USER_FLAIR"
 
-    async def __aiter__(self) -> AsyncGenerator[None, None, None]:
+    async def __aiter__(self) -> AsyncIterator[None]:
         """Abstract method to return flair templates."""
         raise NotImplementedError
 
@@ -1181,7 +1181,7 @@ class SubredditModerationStream:
 
     def edited(
         self, *, only: str | None = None, **stream_options: Any
-    ) -> AsyncGenerator[asyncpraw.models.Comment | asyncpraw.models.Submission, None]:
+    ) -> AsyncGenerator[asyncpraw.models.Comment | asyncpraw.models.Submission]:
         """Yield edited comments and submissions as they become available.
 
         :param only: If specified, one of ``"comments"`` or ``"submissions"`` to yield
@@ -1207,7 +1207,7 @@ class SubredditModerationStream:
         action: str | None = None,
         mod: str | asyncpraw.models.Redditor | None = None,
         **stream_options: Any,
-    ) -> AsyncGenerator[asyncpraw.models.ModAction, None]:
+    ) -> AsyncIterator[asyncpraw.models.ModAction]:
         """Yield moderator log entries as they become available.
 
         :param action: If given, only return log entries for the specified action.
@@ -1239,7 +1239,7 @@ class SubredditModerationStream:
         sort: str | None = None,
         state: str | None = None,
         **stream_options: Any,
-    ) -> AsyncGenerator[ModmailConversation, None]:
+    ) -> AsyncIterator[ModmailConversation]:
         """Yield new-modmail conversations as they become available.
 
         :param other_subreddits: A list of :class:`.Subreddit` instances for which to
@@ -1277,7 +1277,7 @@ class SubredditModerationStream:
 
     def modqueue(
         self, *, only: str | None = None, **stream_options: Any
-    ) -> AsyncGenerator[asyncpraw.models.Comment | asyncpraw.models.Submission, None]:
+    ) -> AsyncIterator[asyncpraw.models.Comment | asyncpraw.models.Submission]:
         r"""Yield :class:`.Comment`\ s and :class:`.Submission`\ s in the modqueue as they become available.
 
         :param only: If specified, one of ``"comments"`` or ``"submissions"`` to yield
@@ -1298,7 +1298,7 @@ class SubredditModerationStream:
 
     def reports(
         self, *, only: str | None = None, **stream_options: Any
-    ) -> AsyncGenerator[asyncpraw.models.Comment | asyncpraw.models.Submission, None]:
+    ) -> AsyncIterator[asyncpraw.models.Comment | asyncpraw.models.Submission]:
         r"""Yield reported :class:`.Comment`\ s and :class:`.Submission`\ s as they become available.
 
         :param only: If specified, one of ``"comments"`` or ``"submissions"`` to yield
@@ -1319,7 +1319,7 @@ class SubredditModerationStream:
 
     def spam(
         self, *, only: str | None = None, **stream_options: Any
-    ) -> AsyncGenerator[asyncpraw.models.Comment | asyncpraw.models.Submission, None]:
+    ) -> AsyncGenerator[asyncpraw.models.Comment | asyncpraw.models.Submission]:
         r"""Yield spam :class:`.Comment`\ s and :class:`.Submission`\ s as they become available.
 
         :param only: If specified, one of ``"comments"`` or ``"submissions"`` to yield
@@ -1338,7 +1338,7 @@ class SubredditModerationStream:
         """
         return stream_generator(self.subreddit.mod.spam, only=only, **stream_options)
 
-    def unmoderated(self, **stream_options: Any) -> AsyncGenerator[asyncpraw.models.Submission, None]:
+    def unmoderated(self, **stream_options: Any) -> AsyncIterator[asyncpraw.models.Submission]:
         r"""Yield unmoderated :class:`.Submission`\ s as they become available.
 
         Keyword arguments are passed to :func:`.stream_generator`.
@@ -1494,7 +1494,7 @@ class SubredditStream:
         """
         self.subreddit = subreddit
 
-    def comments(self, **stream_options: Any) -> AsyncGenerator[asyncpraw.models.Comment, None]:
+    def comments(self, **stream_options: Any) -> AsyncIterator[asyncpraw.models.Comment]:
         """Yield new comments as they become available.
 
         Comments are yielded oldest first. Up to 100 historical comments will initially
@@ -1527,7 +1527,7 @@ class SubredditStream:
         """
         return stream_generator(self.subreddit.comments, **stream_options)
 
-    def submissions(self, **stream_options: Any) -> AsyncGenerator[asyncpraw.models.Submission, None]:
+    def submissions(self, **stream_options: Any) -> AsyncIterator[asyncpraw.models.Submission]:
         r"""Yield new :class:`.Submission`\ s as they become available.
 
         Submissions are yielded oldest first. Up to 100 historical submissions will
@@ -2001,7 +2001,7 @@ class SubredditStylesheet:
 class SubredditWiki:
     """Provides a set of wiki functions to a :class:`.Subreddit`."""
 
-    async def __aiter__(self) -> AsyncGenerator[WikiPage, None]:
+    async def __aiter__(self) -> AsyncIterator[WikiPage]:
         """Iterate through the pages of the wiki.
 
         This method is to be used to discover all wikipages for a subreddit:
@@ -3857,7 +3857,7 @@ class SubredditLinkFlairTemplates(SubredditFlairTemplates):
 
     async def __aiter__(
         self,
-    ) -> AsyncGenerator[dict[str, str | int | bool | list[dict[str, str]]], None]:
+    ) -> AsyncIterator[dict[str, str | int | bool | list[dict[str, str]]]]:
         """Iterate through the link flair templates as a moderator.
 
         For example:
@@ -3959,7 +3959,7 @@ class SubredditLinkFlairTemplates(SubredditFlairTemplates):
 
     async def user_selectable(
         self,
-    ) -> AsyncGenerator[dict[str, str | bool], None]:
+    ) -> AsyncIterator[dict[str, str | bool]]:
         """Iterate through the link flair templates as a regular user.
 
         For example:
@@ -3981,7 +3981,7 @@ class SubredditRedditorFlairTemplates(SubredditFlairTemplates):
 
     async def __aiter__(
         self,
-    ) -> AsyncGenerator[dict[str, str | int | bool | list[dict[str, str]]], None]:
+    ) -> AsyncIterator[dict[str, str | int | bool | list[dict[str, str]]]]:
         """Iterate through the user flair templates.
 
         For example:

asyncpraw/models/reddit/wikipage.py
@@ -11,7 +11,7 @@ from asyncpraw.models.reddit.redditor import Redditor
 from asyncpraw.util.cache import cachedproperty
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator, AsyncIterator
+    from collections.abc import AsyncIterator
 
     import asyncpraw.models
 
@@ -188,7 +188,7 @@ class WikiPage(RedditBase):
         generator_kwargs: dict[str, Any],
         subreddit: asyncpraw.models.Subreddit,
         url: str,
-    ) -> AsyncGenerator[dict[str, Redditor | WikiPage | str | int | bool | None], None]:
+    ) -> AsyncIterator[dict[str, Redditor | WikiPage | str | int | bool | None]]:
         async for revision in ListingGenerator(subreddit._reddit, url, **generator_kwargs):
             if revision["author"] is not None:
                 revision["author"] = Redditor(subreddit._reddit, _data=revision["author"]["data"])
@@ -312,7 +312,7 @@ class WikiPage(RedditBase):
         await page._fetch()
         return page
 
-    def revisions(self, **generator_kwargs: str | int | dict[str, str]) -> AsyncGenerator[WikiPage, None]:
+    def revisions(self, **generator_kwargs: str | int | dict[str, str]) -> AsyncIterator[WikiPage]:
         """Return a :class:`.ListingGenerator` for page revisions.
 
         Additional keyword arguments are passed in the initialization of

asyncpraw/models/util.py
@@ -8,7 +8,7 @@ from collections import OrderedDict
 from typing import TYPE_CHECKING, Any, Callable
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator
+    from collections.abc import AsyncIterator
 
 
 def permissions_string(*, known_permissions: set[str], permissions: list[str] | None) -> str:
@@ -42,7 +42,7 @@ async def stream_generator(
     pause_after: int | None = None,
     skip_existing: bool = False,
     **function_kwargs: Any,
-) -> AsyncGenerator[Any, None]:
+) -> AsyncIterator[Any]:
     """Yield new items from ``function`` as they become available.
 
     :param function: A callable that returns a :class:`.ListingGenerator`, e.g.,